6cef99a0d441b4e3b97cb475b019014680a3651c
luaquecaiu3 <luaquecaiu3@users.noreply.github.com>
2019-08-07 00:31:59 -0300

    a

--- js/angular/functions.js
+++ js/angular/functions.js
@@ -4,7 +4,7 @@ angular.module("helloWorld").controller("helloWorldCtrl", function($scope){
   $scope.message = "Hello Motherfuckers World!";
 });
 
-angular.module("ListaTelefonica", []);
+angular.module("ListaTelefonica", ["ngMessages"]);
 angular.module("ListaTelefonica").controller("ListaTelefonicaCtrl", function($scope){
   $scope.app = "Lista Telefonica";
 
