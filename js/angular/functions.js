ef9760970e4eebccf151be400cb941e106ddfbc4
luaquecaiu3 <luaquecaiu3@users.noreply.github.com>
2019-08-06 22:53:01 -0300

    a

--- js/angular/functions.js
+++ js/angular/functions.js
@@ -12,27 +12,50 @@ angular.module("ListaTelefonica").controller("ListaTelefonicaCtrl", function($sc
   $scope.contatos = [
     {
       nome: "Lua",
-      telefone: "1293456-7890"
+      telefone: "1293456-7890",
+      cor: "blue"
     },
     {
       nome: "Patrick",
-      telefone: "1293456-7890"
+      telefone: "1293456-7890",
+      cor: "red"
     },
     {
       nome: "Ana",
-      telefone: "1293456-7890"
+      telefone: "1293456-7890",
+      cor: "yellow"
     }
   ];
 
   $scope.operadoras = [
-    {      nome: "oi", codigo: 1    },
-      {      nome: "vivo", codigo: 2    },
-        {      nome: "claro", codigo: 3   },
-          {      nome: "tim", codigo: 4    },
-  ]
+    {      nome: "oi", codigo: 1, categoria: 'celular'    },
+    {      nome: "vivo", codigo: 2    , categoria: 'celular'},
+    {      nome: "claro", codigo: 3   , categoria: 'celular'},
+    {      nome: "tim", codigo: 4    , categoria: 'fixo'},
+    {      nome: "gvt", codigo: 5    , categoria: 'fixo'},
+    {      nome: "embratel", codigo:6    , categoria: 'fixo'}
+  ];
+
+  $scope.classe1 = "selecionado";
+  $scope.classe2 = "negrito";
 
   $scope.adicionarContato = function(contato){
     $scope.contatos.push(angular.copy(contato));
     delete $scope.contato;
   };
+
+  $scope.apagarContato = function(contatos){
+    $scope.contatos = contatos.filter(function(contato){
+      if(!contato.selecionado){
+        return contato;
+      }
+    });
+  };
+
+  $scope.isContatoSelecionado = function(contatos){
+    return contatos.some(function (contato){
+      return contato.selecionado;
+    });
+  };
+
 });
