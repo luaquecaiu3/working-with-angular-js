5e664344ae6531df01a3a410f0170bc2d834e2cd
luaquecaiu3 <luaquecaiu3@users.noreply.github.com>
2019-08-11 15:33:27 -0300

    a

---
+++ js/functions2.js
@@ -0,0 +1,30 @@
+var app = angular.module('main', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.when('/',{
+    templateUrl: './components/home/html',
+    controller: 'homeCtrl'
+  }).when('/login',{
+    templateUrl: './components.login.html',
+    controller: 'loginCtrl'
+  }).otherwise({
+    template: '404'
+  })
+});
+
+app.controller('homeCtrl', function($scope){
+  $scope.goToLogin = function(){
+    $location.path('/login');
+  }
+  $scope.register = function(){
+    $location.path('/register');
+  }
+});
+
+app.controller('loginCtrl', function($scope){
+  $scope.login = function(){
+    var username = $scope.username;
+    var password = $scope.password;
+    
+  }
+});
